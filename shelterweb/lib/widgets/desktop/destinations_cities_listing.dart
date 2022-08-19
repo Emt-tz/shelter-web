bea51d79c44cd8652870ccebc6b185207c137508
Emmanuel Mtera <peterkelvin16@gmail.com>
2022-08-19 16:32:57 +0300

    Update destinations_cities_listing.dart

--- shelterweb/lib/widgets/desktop/destinations_cities_listing.dart
+++ shelterweb/lib/widgets/desktop/destinations_cities_listing.dart
@@ -23,7 +23,8 @@ class _DestinationCitiesListingState extends State<DestinationCitiesListing> {
         color: Color(0xFFEEEEEE),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Padding(
+      child: 
+      Padding(
         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
         child: Column(
           mainAxisSize: MainAxisSize.max,
